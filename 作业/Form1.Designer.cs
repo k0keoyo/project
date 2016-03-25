bc224f7e8e1fdd81b7884e1646a10ae25a7b0d0a
k0keoyo <k0pwn_0110@sina.cn>
2016-03-25 20:14:11 +0800

    project
    project

---
+++ 作业/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace 作业
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.Sex = new System.Windows.Forms.TextBox();
+            this.Score = new System.Windows.Forms.TextBox();
+            this.Rank = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名：";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(31, 70);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(780, 417);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "性别：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(387, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "分数：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(572, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "名次：";
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(73, 30);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(100, 21);
+            this.Name.TabIndex = 5;
+            // 
+            // Sex
+            // 
+            this.Sex.Location = new System.Drawing.Point(248, 30);
+            this.Sex.Name = "Sex";
+            this.Sex.Size = new System.Drawing.Size(100, 21);
+            this.Sex.TabIndex = 5;
+            // 
+            // Score
+            // 
+            this.Score.Location = new System.Drawing.Point(433, 30);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(100, 21);
+            this.Score.TabIndex = 5;
+            // 
+            // Rank
+            // 
+            this.Rank.Location = new System.Drawing.Point(618, 30);
+            this.Rank.Name = "Rank";
+            this.Rank.Size = new System.Drawing.Size(100, 21);
+            this.Rank.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(734, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "添加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "姓名";
+            this.columnHeader1.Width = 157;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "性别";
+            this.columnHeader2.Width = 153;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "分数";
+            this.columnHeader3.Width = 182;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "名次";
+            this.columnHeader4.Width = 257;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 522);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Rank);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.Sex);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Text = "作业";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.TextBox Sex;
+        private System.Windows.Forms.TextBox Score;
+        private System.Windows.Forms.TextBox Rank;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ 作业/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 作业
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Name.Text = "";
+            Sex.Text = "";
+            Score.Text = "";
+            Rank.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string m_strName = Name.Text;
+            string m_strSex = Sex.Text;
+            string m_strScore = Score.Text;
+            string m_strRank = Rank.Text;
+            try{
+                ListViewItem m_listviewItem1 = new ListViewItem();
+                m_listviewItem1.SubItems[0].Text = m_strName;
+                //m_listviewItem1.SubItems.Add(m_strName);
+                m_listviewItem1.SubItems.Add(m_strSex);
+                m_listviewItem1.SubItems.Add(m_strScore);
+                m_listviewItem1.SubItems.Add(m_strRank);
+                listView1.Items.Add(m_listviewItem1);
+                MessageBox.Show("添加成功");
+            }
+            catch
+            {
+                MessageBox.Show("添加失败！");
+            }
+
+
+        }
+    }
+}
